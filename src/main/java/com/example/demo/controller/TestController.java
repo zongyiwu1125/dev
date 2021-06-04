daa2aba5e05c26d058a77e61000dfa65fe1d9afd
zongyiwu1125 <1805722168@qq.com>
2021-06-04 14:27:47 +0800

    测试第一阶段

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test1")
+    public String test1(){
+        return "1111";
+    }
+}
