3221daeb97677f9bd8e4b439d1362c9f7c638a7a
zongyiwu1125 <1805722168@qq.com>
2021-06-08 20:45:03 +0800

    修改测试

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -6,15 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping("/test1")
-    public String test1(){
-        return "1111";
-    }
-
-    @RequestMapping("/test3")
-    public String test3(){
-        return "3333";
-    }
 
     @RequestMapping("/test2")
     public String test2(){
